6a7016fb945b6aa9a75831ad73ea743c376d356e
Walter Gammarota <waltergammarota@MacBook-Air-de-Walter.local>
2025-02-19 14:00:14 -0300

    wip

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:1337/api'
 })
--- frontend/src/api/stories.js
+++ frontend/src/api/stories.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from './axios'
 
 export default {
     async getStories(page = 1, limit = 10, sort = 'createdAt:desc') {
