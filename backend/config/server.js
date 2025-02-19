33c693817f115293942c936279dc8c8b73fa0350
Walter Gammarota <waltergammarota@MacBook-Air-de-Walter.local>
2025-02-19 13:54:35 -0300

    wip

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('STRAPI_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
