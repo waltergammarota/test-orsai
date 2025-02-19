84ff98ef6ca505c53c2ef50791e8396909e9eb30
Walter Gammarota <waltergammarota@MacBook-Air-de-Walter.local>
2025-02-19 13:42:12 -0300

    wip

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,2 @@
 const strapi = require('@strapi/strapi');
-
-module.exports = (req, res) => {
-    if (!strapi.isLoaded) {
-        strapi({ distDir: './build' }).start().then(() => {
-            strapi.server.mount();
-            console.log('Strapi is running...');
-        });
-    }
-
-    strapi.server.emit('request', req, res);
-};
+strapi.createStrapi(/* {...} */).start();
