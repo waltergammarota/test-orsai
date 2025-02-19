b9835078a307ff4841f7c38fffb8d95a05b0618f
Walter Gammarota <waltergammarota@MacBook-Air-de-Walter.local>
2025-02-19 12:03:18 -0300

    Vercel

--- frontend/src/api/stories.js
+++ frontend/src/api/stories.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:1337/api'
-
 export default {
     async getStories(page = 1, limit = 10, sort = 'createdAt:desc') {
-        const response = await axios.get(`${API_URL}/stories`, {
+        const response = await axios.get(`/stories`, {
             params: {
                 'pagination[page]': page,
                 'pagination[pageSize]': limit,
@@ -15,7 +13,7 @@ export default {
     },
 
     async getAllStories() {
-        const response = await axios.get(`${API_URL}/stories`, {
+        const response = await axios.get(`/stories`, {
             params: {
                 'pagination[pageSize]': 10000 // Asumimos que no hay más de 10000 historias
             }
